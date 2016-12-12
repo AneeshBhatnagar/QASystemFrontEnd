cf24bdd0645094616c4656504c6249df8233081e
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2016-12-12 00:56:14 -0500

    Added check for fail questions

--- js/search-script.js
+++ js/search-script.js
@@ -1,18 +1,3 @@
-/*var jsonData = '{"answer":"Narendra Modi","hashtag_counts":"10,20,15,35","hashtag_labels":"Aneesh,Testing,Blah,Blah","tweet_count":2,"tweets":[{"profile_pic_url":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png","screen_name":"Aneesh Bhatnagar","tweet_text":"Hello there!","user_name":"SlashAneesh"},{"profile_pic_url":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png","screen_name":"Aneesh Testing","tweet_text":"Hello therew2!","user_name":"SlashAnTs"}]}';
-var jsonData2 = '{"answer":"Narendra Modi","hashtag_counts":"[10,20,15,35]","hashtag_labels":"[Aneesh,Testing,Blah,Blah]","tweet_count":2,"tweets":[{"profile_pic_url":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png","screen_name":"Aneesh Bhatnagar","tweet_text":"Hello there!","user_name":"SlashAneesh"},{"profile_pic_url":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png","screen_name":"Aneesh Testing","tweet_text":"Hello therew2!","user_name":"SlashAnTs"}],"wordcloud":[{"text":"Aneesh","weight":5},{"text":"Bhatnagar","weight":7},{"text":"Test","weight":10}]}';
-var count = [65, 59, 80, 81, 56, 35];
-var words = [
-		  {text: "Lorem", weight: 13},
-		  {text: "Ipsum", weight: 10.5},
-		  {text: "Dolor", weight: 9.4},
-		  {text: "Sit", weight: 8},
-		  {text: "Amet", weight: 6.2},
-		  {text: "Aneesh", weight: 5},
-		  {text: "Testing", weight: 5},
-		  {text: "SHIT", weight: 4}
-		];
-*/
-
 //Replace +, ? etc
 var queryTerm = getURLParameter();
 
@@ -37,24 +22,30 @@ $.ajax({
 		});
 
 function parseData(data){
-	if(data.answer == '' || data.answer == ' '){
-		$("#result-box").css('display','none');
+	if(data.status == 'fail'){
+		$("#search-answer").html(data.error);
+		$("#result-count").html('0');
+		$("#right-col").css("display", "none");
 	}
-	$("#search-answer").html(data.answer);
-	$("#result-count").html(data.tweet_count);
-	createWordCloud(data.wordcloud);
-	createChartForHashtags(data.hashtags.labels, data.hashtags.count);
-	createSentimentChart(data.sentiment.labels,data.sentiment.count);
-	var tweets = data.tweets;
-	var profilePic, name, username, tweet, appendData;
-	for(var i=0; i<tweets.length;i++){
-		profilePic = tweets[i].displaypic[0];
-		name = tweets[i].name[0];
-		username = tweets[i].username[0];
-		tweet = tweets[i].text;
-		$("#tweets-row").append(returnAppendData(profilePic, name, username, tweet));
+	else{
+		if(data.answer == '' || data.answer == ' '){
+			$("#result-box").css('display','none');
+		}
+		$("#search-answer").html(data.answer);
+		$("#result-count").html(data.tweet_count);
+		createWordCloud(data.wordcloud);
+		createChartForHashtags(data.hashtags.labels, data.hashtags.count);
+		createSentimentChart(data.sentiment.labels,data.sentiment.count);
+		var tweets = data.tweets;
+		var profilePic, name, username, tweet, appendData;
+		for(var i=0; i<tweets.length;i++){
+			profilePic = tweets[i].displaypic[0];
+			name = tweets[i].name[0];
+			username = tweets[i].username[0];
+			tweet = tweets[i].text;
+			$("#tweets-row").append(returnAppendData(profilePic, name, username, tweet));
+		}
 	}
-
 	$("#preloader").fadeOut();
 	$.ajax({
 	    'url': "http://35.162.43.82:8888/getAutoComplete",
@@ -71,6 +62,7 @@ function parseData(data){
 	    'dataType': 'jsonp',
 	    'jsonp': 'callback'
 	});
+	
 }
 
 function returnAppendData(pic, name, username, text){
